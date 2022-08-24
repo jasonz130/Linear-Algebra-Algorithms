96647c949fa1ca4b727c774a6d0574904428b6be
Jason Zhang <87998934+jasonz130@users.noreply.github.com>
2022-08-24 18:25:40 -0400

    Update GaussianElimination.c

--- GaussianElimination.c
+++ GaussianElimination.c
@@ -2,162 +2,133 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-
-int **subtractRows(int **matrix, int rowSize, int minuendRow, int subtrahendRow){
-    for (int i = 0; i < rowSize; i++){
-        matrix[minuendRow][i] = matrix[minuendRow][i] - matrix[subtrahendRow][i];
-    }
-    return matrix;
-
-}
-
-int **addRows(int **matrix, int rowSize, int summandRow, int rowToAdd){
-    for (int i = 0; i < rowSize;  i++){
-        matrix[summandRow][i] = matrix[summandRow][i] + matrix [rowToAdd][i];
-    }
-    return matrix;
-
-}
-
-int **multiplyRow(int **matrix, int rowSize, int rowMultiply, int factor){
-    for (int i = 0; i < rowSize; i++){
-        matrix[rowMultiply][i] = matrix[rowMultiply][i] * factor;
-    }
-    return matrix;
-
-}
-
-int **swapRows(int **matrix, int rowSize, int row1, int row2){
-    int temp;
-    for (int i = 0; i < rowSize; i++){
-        temp = matrix[row1][i];
-        matrix[row1][i] = matrix[row2][i];
-        matrix[row2][i] = temp;
-    }
-    return matrix;
-}
-
-int **createMatrix(int rowSize, int colSize){
-    
-    // DYNAMIC ALLOCATE 2D ARRAY
-    int **matrix = (int**)malloc(rowSize * sizeof(int*));
-    for (int i = 0; i < colSize; i++){
-        matrix[i] = (int*)malloc(colSize * sizeof(int));
-    }
-
-    // STORE MATRIX ENTRIES
-    for (int row = 0; row < rowSize; row++){
-        for (int col = 0; col < colSize; col++){
-            printf("Row %d Column %d = ", row, col);
-            scanf("%d", &matrix[row][col]);
+void printMatrix(double **matrix, int row, int col){
+    for (int i = 0; i < row; i++){
+        printf("\n");
+        for (int j = 0; j < col; j++){
+            printf("%lf ", matrix[i][j]);
         }
     }
-    return matrix;
-}
-
-bool isZeroMatrix(int **matrix, int rowSize, int colSize){
-    for (int row = 0; row < rowSize; row++){
-        for (int col = 0; col < colSize; col++){
-            if (matrix[row][col] != 0){;
-                return false;
-            }
-        }
-    }
-    return true;
-}
-
-int **gaussianEliminate(int **matrix, int rowSize, int colSize){
-    
-    // ZERO MATRIX
-    if (isZeroMatrix(matrix, rowSize, colSize)){
-        return matrix;
-    }
-
-    // NONZERO MATRIX
-    // find first column from left containing a nonzero
-    int col = 0, row;
-    bool nonZero = false;
-
-    int j;
-    bool condition;
-
-    int savedNumber;
-
-    for (int iteration = 0; iteration < rowSize; iteration++){
-        // while (col < colSize && nonZero == false){
-        //     row = iteration;
-        //     while (row < rowSize && nonZero == false){
-        //         if (matrix[row][col] != 0){
-        //             nonZero = true;
-        //         }
-        //         else{
-        //             row++;
-        //         }
-        //     }
-        //     col++;
-        // }
-
-        // swapRows(matrix, rowSize, row, iteration);    
-        
-        j = 0;
-        condition = false;
-
-        while (j < colSize && condition == false){
-            if (matrix[iteration][j] != 0){
-                multiplyRow(matrix, rowSize, iteration, 1 / matrix[iteration][j]);  // achieve leading one
-                condition = true;
-            }
-            else{
-                j++;
-            }
-        }
-        
-        for (int k = 0; k < rowSize; k++){          // iterate through each row
-            if (k != iteration){
-                if (matrix[k][j] != 0){
-                    savedNumber = matrix[k][j];
-                    multiplyRow(matrix, rowSize, iteration, savedNumber);
-                    subtractRows(matrix, rowSize, k, iteration);
-                    multiplyRow(matrix, rowSize, iteration, 1 / savedNumber); // unmultiply
-                }
-
-            }
-            // else { do nothing }
-        }
-    }
-
-    return matrix;
-}
-
-
-int main(){
-
-    // PROMPT MATRIX SIZE
-    int rowSize, colSize;
-    printf("Enter the row size: ");
-    scanf("%d", &rowSize);
     printf("\n");
+}
 
-    printf("Enter the col size: ");
-    scanf("%d", &colSize);
-    printf("\n");
+double **createMatrix(int row, int col){
+    double** matrix = (double **)malloc(row * sizeof(double *));
+    for (int i = 0; i < row; i++){
+        matrix[i] = (double *)malloc(col * sizeof(double));
+    }
+    return matrix;
+}
 
-    // CREATE MATRIX 2D DYNAMIC ARRAY
-    int **matrix = createMatrix(rowSize, colSize);
 
-    // APPLY GAUSSIAN ELIMINATION TO BRING MATRIX INTO RREF
-    gaussianEliminate(matrix, rowSize, colSize);
-
-    // PRINT RREF MATRIX
-    printf("\nRREF:\n");
-    for (int row = 0; row < rowSize; row++){
-        for (int col = 0; col < colSize; col++){
-            printf("%d ", matrix[row][col]);
+void fillMatrix(double **matrix, int row, int col){
+    for (int i = 0; i < row; i++){
+        for (int j = 0; j < col; j++){
+            printf("Enter entry row: %d, col: %d: ", i + 1, j + 1);
+            scanf("%lf", &matrix[i][j]);
         }
         printf("\n");
     }
+}
 
-    free(matrix);
+void swapRows(double **matrix, int row1, int row2, int col){
+    double temp;
+    for (int i = 0; i < col; i++){
+        temp = matrix[row2][i];
+        matrix[row2][i] = matrix[row1][i];
+        matrix[row1][i] = temp;
+    }
+}
 
+void swapRowOfZeros(double **matrix, int row, int col){
+    int counter = 0;
+    for (int i = 0; i < row; i++){
+        for (int j = 0; j < col; j++){
+            if (matrix[i][j] < 0.001 && matrix[i][j] > -0.001){
+                counter++;
+            }
+        }
+        if (counter == col){
+            for (int idx = 0; idx < row - i - 1; idx++){
+                swapRows(matrix, idx + i, idx + i + 1, col);
+            }
+            printf("\nswapRowofZeros\n");
+        }
+        counter = 0;
+    }
+    
+}
+
+void columnsToZero(double **matrix, int row, int col, int entry_row, int entry_col){
+    double multiplier;
+    for (int i = 0; i < row; i++){
+        if (i != entry_row && matrix[i][entry_col] != 0){
+            multiplier = matrix[i][entry_col];
+            
+            for (int j = 0; j < col; j++){
+                matrix[i][j] = matrix[i][j] - multiplier * matrix[entry_row][j];
+
+            }
+        }
+    }
+}
+
+void gaussianEliminateActual(double **matrix, int row, int col){
+    swapRowOfZeros(matrix, row, col);
+    int rows_counter = 0;
+    int j;
+    bool found;
+    double saved_entry;
+    for (int i = 0; i < col; i++){
+        j = rows_counter;
+        found = false;
+
+        while(j < row && found == false){
+            if (matrix[j][i] > 0.001 || matrix[j][i] < -0.001){         // if nonzero entry
+                if (rows_counter != j){                                     // don't want to swap the same row
+                    swapRows(matrix, rows_counter, j, col);
+                }
+
+                // divide by 1 ... etc.                                 // divide nonzero entry by 1
+                saved_entry = matrix[rows_counter][i];
+                for (int i_col = 0; i_col < col; i_col++){              // add / subtract s.t. other entries in the column is 0
+                    matrix[rows_counter][i_col] = matrix[rows_counter][i_col] / saved_entry;
+                    
+                }                                                        
+                columnsToZero(matrix, row, col, rows_counter, i);
+
+                rows_counter++;                 // default row to swap
+                found = true;                   // exit while loop once nonzero entry is found (exit upon finding the first leading 1)
+            }
+            
+            else {
+                j++;
+            }
+            
+            // if no nonzero entry, move to next column by exiting while loop and returning to outer for loop
+
+        }
+    }
+}
+
+int main(){
+    /* PROMPT VECTOR ROW AND COL */
+    int row;
+    printf("Enter the number of rows: ");
+    scanf("%d", &row);
+    printf("\n");
+
+    int col;
+    printf("Enter the number of columns: ");
+    scanf("%d", &col);
+    printf("\n");
+
+    /* INIT MATRIX ENTRIES */
+    double **matrix = createMatrix(row, col);
+    fillMatrix(matrix, row, col);
+    printMatrix(matrix, row , col);
+
+    gaussianEliminateActual(matrix, row, col);
+    printMatrix(matrix, row, col);
     return 0;
 }
